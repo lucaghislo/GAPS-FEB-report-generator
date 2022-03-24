76c5acea7fa9d7c91a6f2d4a358f31d6fb673d92
Luca Ghislotti <l.ghislotti@studenti.unibg.it>
2022-03-24 09:24:05 +0100

    workind pedestal script

--- FEB_py_script/FEB_pedestal_calc.py
+++ FEB_py_script/FEB_pedestal_calc.py
@@ -10,6 +10,15 @@ file_ped = os.path.join(dir_ped, '../script_files/Pedestals.dat')
 with open(file_ped) as f:
     lines_tm = f.readlines()
 
-for i in lines_tm[45:]:
+sum = 0
+count = 0
+
+for i in lines_tm[1:]:
     str_tm = i.split("\t")
-    print(str_tm)
+
+    if(int(str_tm[1])==6):
+        sum = sum + float(str_tm[3])
+        count = count + 1
+
+print("\n*** PEDESTAL DISPERSION ***")
+print("\nPedestal dispersion [ADC]: " + str(round(sum/count, 3)) + "\n")
